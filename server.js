50497314970672d72e7671a68800d68c83309302
heartziq <heartziq@gmail.com>
2016-05-15 15:13:04 +0800

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ var express = require('express'); //import express api
 
 //create my app
 var app = express();
+const PORT = process.env.PORT || 3000; //access ENVIRONMENT VARIABLE handled by node.js
+
+//no port assigned, (means local: 3000)
+
+//check if not http req, redirect to http - openWeatherMap is http-only
+app.use(function (req, res, next){
+	if (req.headers['x-forwarded-proto'] === 'http'){
+		next();
+	} else {
+		res.redirect('http//' + req.hostname + req.url);
+	}
+});
 
 app.use(express.static('public')); //adds functionality to your express application
 //specify the folder that I want to expose to the web server
-app.listen(3000, function (){
-	console.log('Express Server is Up on port 3000');
+
+app.listen(PORT, function (){
+	console.log('Express Server is Up on port ' + PORT);
 });
