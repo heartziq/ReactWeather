f7977f54945374d25ad6dc939da59315d9914339
heartziq <heartziq@gmail.com>
2016-05-15 15:04:21 +0800

    minor change

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 //one-liner (expression)
 var Example = () => (<div><h3>More examples here!</h3>
-<p>welcome to example page</p>
+<p>welcome to example page, again!</p>
 </div>);
 
 module.exports = Example;
