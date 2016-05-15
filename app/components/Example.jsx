5376687b78c188c3852a64483498d4ec3c545da6
heartziq <heartziq@gmail.com>
2016-05-15 14:51:07 +0800

    added p tag

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,6 +1,8 @@
 var React = require('react');
 
 //one-liner (expression)
-var Example = () => (<h3>More examples here!</h3>);
+var Example = () => (<div><h3>More examples here!</h3>
+<p>welcome to example page</p>
+</div>);
 
 module.exports = Example;
