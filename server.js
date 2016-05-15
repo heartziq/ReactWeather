e4869d1ab8aa8d168d0dcbc94078f08e9485c9d3
heartziq <heartziq@gmail.com>
2016-05-15 15:15:49 +0800

    url corrected

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(function (req, res, next){
 	if (req.headers['x-forwarded-proto'] === 'http'){
 		next();
 	} else {
-		res.redirect('http//' + req.hostname + req.url);
+		res.redirect('http://' + req.hostname + req.url);
 	}
 });
 
